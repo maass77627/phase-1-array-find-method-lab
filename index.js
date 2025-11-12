6bd7dd0b1df5ea7ae6c567dd3e607fc6868aba21
kelly_maass <kmaass77627@gmail.com>
2025-11-12 15:13:38 -0600

    done

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-// code your solution here
 
-const record = [
-    { year: "2015", result: "W"},
-    { year: "2014", result: "N/A"},
-    { year: "2013", result: "L"},
-    
-  ]
+
+
 
 function superbowlWin(record) {
   
@@ -22,4 +17,12 @@ function superbowlWin(record) {
  }
 
  superbowlWin(record)
+
+
+
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
 
